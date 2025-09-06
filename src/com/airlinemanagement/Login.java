b731344d7bcbe59a6c3e26f906ca5fb3ca9c9265
RSM Harsha <postboxofharsha@gmail.com>
2025-09-06 23:39:37 +0530

    "Updated code in Login Page"

--- src/com/airlinemanagement/Login.java
+++ src/com/airlinemanagement/Login.java
@@ -14,55 +14,63 @@ public class Login extends JFrame implements ActionListener {
         getContentPane().setBackground(Color.white);
         setLayout(null);
 
-        JLabel lblusername = new JLabel("Username:");
-        lblusername.setBounds(20, 20, 100, 20);
+        // Username row
+        JLabel lblusername = new JLabel("Username :");
+        lblusername.setBounds(110, 40, 100, 25);
         add(lblusername);
 
         tfusername = new JTextField();
-        tfusername.setBounds(130, 20, 200, 20);
+        tfusername.setBounds(180, 40, 150, 25);
         tfusername.setToolTipText("Enter your username");
         add(tfusername);
 
-        JLabel lblpassword = new JLabel("Password:");
-        lblpassword.setBounds(20, 60, 100, 20);
+        // Password row
+        JLabel lblpassword = new JLabel("Password :");
+        lblpassword.setBounds(110, 80, 100, 25);
         add(lblpassword);
 
         tfpassword = new JPasswordField();
-        tfpassword.setBounds(130, 60, 200, 20);
+        tfpassword.setBounds(180, 80, 150, 25);
         tfpassword.setToolTipText("Enter your password");
         add(tfpassword);
 
+        // Buttons
         reset = new JButton("Reset");
-        reset.setBounds(40, 120, 120, 25);
+        reset.setBounds(50, 140, 100, 30);
         reset.addActionListener(this);
         add(reset);
 
         submit = new JButton("Login");
-        submit.setBounds(190, 120, 120, 25);
+        submit.setBounds(155, 140, 100, 30);
         submit.addActionListener(this);
         add(submit);
 
         close = new JButton("Exit");
-        close.setBounds(120, 160, 120, 25);
+        close.setBounds(260, 140, 100, 30);
         close.addActionListener(this);
         add(close);
 
-        setSize(400, 250);
+        // Frame setup
+        setSize(420, 250);
         setLocation(600, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
+
     @Override
     public void actionPerformed(ActionEvent ae){
-        if(ae.getSource() == "submit"){
-            
+        if(ae.getSource() == submit){
+            // Here you can add login validation later
+            JOptionPane.showMessageDialog(this, "Login button clicked!");
         } else if (ae.getSource() == close) {
             setVisible(false);
+            dispose(); // better cleanup
         } else if (ae.getSource() == reset) {
             tfusername.setText("");
             tfpassword.setText("");
         }
     }
+
     public static void main(String[] args){
         new Login();
     }
